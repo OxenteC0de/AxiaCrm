352e62c8de7cb22a00d99493aadeab6df6118976
William Martins <134098387+willmartinsss@users.noreply.github.com>
2025-10-02 10:26:42 -0300

    william/Oportunidade

--- src/Oportunidade/service/oportunidade.service.ts
+++ src/Oportunidade/service/oportunidade.service.ts
@@ -1,3 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Oportunidade } from '../entities/oportunidade.entity';
+
+@Injectable()
 export class OportunidadeService {
-  
+  constructor(
+    @InjectRepository(Oportunidade)
+    private readonly oportunidadeRepository: Repository<Oportunidade>,
+  ) {}
+
+  async create(oportunidade: Partial<Oportunidade>): Promise<Oportunidade> {
+    const newOportunidade = this.oportunidadeRepository.create(oportunidade);
+    return this.oportunidadeRepository.save(newOportunidade);
+  }
+
+  async findAll(): Promise<Oportunidade[]> {
+    return this.oportunidadeRepository.find({
+      relations: ['cliente', 'usuario'],
+    });
+  }
+
+  async findOne(id: number): Promise<Oportunidade> {
+    return this.oportunidadeRepository.findOne({
+      where: { id },
+      relations: ['cliente', 'usuario'],
+    });
+  }
+
+  async update(
+    id: number,
+    updateData: Partial<Oportunidade>,
+  ): Promise<Oportunidade> {
+    await this.oportunidadeRepository.update(id, updateData);
+    return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.oportunidadeRepository.delete(id);
+  }
 }
